06c5968fe034dc2fa5f5a61d43acadaf9ab423d4
UditVerma22 <vermaudit724@gmail.com>
2025-04-18 12:25:52 +0530

    Add files via upload

---
+++ OPERATORS.java
@@ -0,0 +1,22 @@
+// OPERATORS
+
+// UNARY OPERATOR BINARY OPERATOR TERNARY OPERATOR
+
+// ++ (Increment) Arithmetical Operators Conditional Operators
+// -- (Decrement) +,-,*,/,% ? :
+// a++ (Post Increment) Relational Operators
+// ++a (Pre Increment) <,<=,>,>=,!=,==
+// a-- (Post Decrement) Assignment Oerator
+// --a (Pre Decrement) =
+// Bitwise Operators
+// &,|, ^, <<, >>, >>>
+// AND,OR,X-OR,Left Shift,Right Shift,Right Shift with ero fill
+// Logical Operators
+// && Logical AND // Short Circuited AND
+// || Logical OR // Short Circuited OR
+
+// Compound Operators
+// +=,-=,*=,/=,%=
+// ------------------------
+
+// a++ => a=a+1;
