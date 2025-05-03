c954e6234befba4448bf27cea0dc28e5af42dc13
UditVerma22 <vermaudit724@gmail.com>
2025-05-03 20:30:49 +0530

    Add files via upload

---
+++ PrintyournameonconsoleTENTIMESusingWHILELOOP.java
@@ -0,0 +1,14 @@
+// WHILE LOOP
+
+// WAP tp print your name on console for the ten times
+
+class PrintyournameonconsoleTENTIMESusingWHILELOOP {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("UDIT");
+            i++;
+        }
+    }
+
+}
