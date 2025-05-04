de53a0207476044ea19bd4ac0073d32ddf828a01
UditVerma22 <vermaudit724@gmail.com>
2025-05-04 15:06:06 +0530

    Add files via upload

---
+++ Series2.java
@@ -0,0 +1,11 @@
+// WAP to print the following series :- 2 4 6 8 10 12 14 16 18 20
+
+class Series2 {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(2 * i + " ");
+            i++;
+        }
+    }
+}
---
+++ Series3.java
@@ -0,0 +1,11 @@
+// WAP to print the following series :- 1 4 9 16 25 36 49 64 81 100
+class Series3 {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i * i + " ");
+            i++;
+        }
+    }
+
+}
