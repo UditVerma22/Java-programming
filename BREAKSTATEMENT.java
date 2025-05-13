56faab90faf0919f5b9acf714da6ee920cfaa006
UditVerma22 <vermaudit724@gmail.com>
2025-05-13 23:34:47 +0530

    Add files via upload

---
+++ BREAKSTATEMENT.java
@@ -0,0 +1,16 @@
+// Example
+class BREAKSTATEMENT {
+    public static void main(String args[]) {
+        int i = 1;
+        for (;;) {
+            if (i > 10) {
+                break;
+            }
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+
+}
+
+// Har vo natural number jiske sif do factors ho usko prime number bolte h ....
---
+++ FORLOOP2.java
@@ -0,0 +1,11 @@
+// Example 2
+public class FORLOOP2 {
+    public static void main(String args[]) {
+        int i = 1;
+        for (; i <= 10;) { // while(i<=10){
+            System.out.println(i + " "); // System.out.println(i+" ");
+            i++; // i++;
+        } // }
+    } // }
+
+} // }
