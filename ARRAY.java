92274baa912a41ba4244785eaccc9ff675e754ea
UditVerma22 <vermaudit724@gmail.com>
2025-05-18 22:33:38 +0530

    Add files via upload

---
+++ ARRAY.java
@@ -0,0 +1,10 @@
+// ARRAY-
+
+// An array is a collection of similar type of data elements.
+
+// In java we can declare an array in two ways -
+
+// a) int a[]={10,20,30,40,50}
+// b) int a[]=new int[10];
+
+// NOTE :- In java every array is an object
---
+++ DOWHILEloop.java
@@ -0,0 +1,13 @@
+// Do-While Loop
+
+class DOWHILEloop {
+    public static void main(String args[]) {
+        int i = 1;
+        do { // while(i<=10){
+            // System.out.print(i+" ");
+            // i++;
+            // }
+            System.out.print(i + " ");
+        } while (i <= 10);
+    }
+}
